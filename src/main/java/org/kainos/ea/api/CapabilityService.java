8d514941a326bdaf7fcf1fc0298435701e5bb108
Darragh <darragh.haughey@kainos.com>
2023-11-15 11:12:32 +0000

    Set up get all capabilities with swagger integration

---
+++ src/main/java/org/kainos/ea/api/CapabilityService.java
@@ -0,0 +1,26 @@
+package org.kainos.ea.api;
+
+import org.kainos.ea.cli.Capability;
+import org.kainos.ea.cli.JobRole;
+import org.kainos.ea.client.ActionFailedException;
+import org.kainos.ea.db.CapabilityDao;
+import org.kainos.ea.db.DatabaseConnector;
+import org.kainos.ea.db.JobRoleDao;
+
+import java.util.List;
+
+public class CapabilityService {
+
+    private final DatabaseConnector databaseConnector;
+    private final CapabilityDao capabilityDao;
+
+    public CapabilityService(DatabaseConnector databaseConnector, CapabilityDao capabilityDao) {
+        this.databaseConnector = databaseConnector;
+        this.capabilityDao = capabilityDao;
+    }
+
+    public List<Capability> getAllCapabilities() throws ActionFailedException {
+        return capabilityDao.getAllCapabilities(databaseConnector.getConnection());
+    }
+
+}

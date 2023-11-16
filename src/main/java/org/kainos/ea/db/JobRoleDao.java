a08797ad935d425e74a28b331ceb77cf1062f381
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-11-16 11:35:59 +0000

    WIP

--- src/main/java/org/kainos/ea/db/JobRoleDao.java
+++ src/main/java/org/kainos/ea/db/JobRoleDao.java
@@ -1,6 +1,7 @@
 package org.kainos.ea.db;
 
 import org.kainos.ea.cli.JobRole;
+import org.kainos.ea.cli.JobRoleRequest;
 import org.kainos.ea.client.ActionFailedException;
 
 import java.sql.Connection;
@@ -38,5 +39,29 @@ public class JobRoleDao {
             throw new ActionFailedException("Failed to get Job Roles");
         }
 
+    }
+
+    public int createJobRole(Connection c, JobRoleRequest jobRoleRequest) throws ActionFailedException {
+        try (Statement st = c.createStatement()) {
+            String queryString = "INSERT INTO job_role (job_role_id, job_role_title, capability_id, band_id, job_role_sharepoint_link) " +
+            "VALUES (?, ?, ?, ?, ?);";
+
+            ResultSet rs = st.executeQuery(queryString);
+
+            while (rs.next()) {
+                JobRole jobRole = new JobRole(
+                        rs.getInt("job_role_id"),
+                        rs.getString("job_role_title"),
+                        rs.getString("band_name")
+                );
+
+                jobRoleList.add(jobRole);
+            }
+
+            return jobRoleList;
+        } catch (SQLException e){
+            System.err.println(e.getMessage());
+            throw new ActionFailedException("Failed to get Job Roles");
+        }
     }
 }

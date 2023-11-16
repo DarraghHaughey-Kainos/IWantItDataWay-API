430fa3ecb0d66d63395b0ed34fce4b0f82e44752
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-11-16 09:32:38 +0000

    SQL script update

--- sql-scripts/US004-view-band-level.sql
+++ sql-scripts/US004-view-band-level.sql
@@ -11,6 +11,9 @@ INSERT INTO band (band_name) VALUES ("Apprentice"), ("Trainee"), ("Associate"), 
 ALTER TABLE job_role
 ADD band_id tinyint unsigned;
 
+ALTER TABLE job_role
+ADD FOREIGN KEY (band_id) REFERENCES band(band_id);
+
 UPDATE job_role SET band_id = 3 WHERE job_role_id = 1;
 UPDATE job_role SET band_id = 5 WHERE job_role_id = 2;
 UPDATE job_role SET band_id = 4 WHERE job_role_id = 3;
@@ -23,3 +26,7 @@ UPDATE job_role SET band_id = 2 WHERE job_role_id = 9;
 UPDATE job_role SET band_id = 5 WHERE job_role_id = 10;
 
 ALTER TABLE job_role MODIFY COLUMN band_id tinyint unsigned NOT NULL;
+
+SELECT job_role_id, job_role_title, band_name 
+FROM job_role
+LEFT JOIN band USING(band_id);

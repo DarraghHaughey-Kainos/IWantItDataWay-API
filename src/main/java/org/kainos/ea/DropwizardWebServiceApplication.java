d1b2ccf655cda305a33023f48af81ee8fe05f7f1
Stephen Hanna <stephenh126_@hotmail.co.uk>
2023-11-16 12:02:58 +0000

    small update

--- src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
+++ src/main/java/org/kainos/ea/DropwizardWebServiceApplication.java
@@ -5,7 +5,9 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+import org.kainos.ea.api.BandService;
 import org.kainos.ea.api.JobRoleService;
+import org.kainos.ea.db.BandDao;
 import org.kainos.ea.db.DatabaseConnector;
 import org.kainos.ea.db.JobRoleDao;
 import org.kainos.ea.resources.BandController;
@@ -13,11 +15,13 @@ import org.kainos.ea.resources.HelloWorldController;
 import org.kainos.ea.resources.JobRoleController;
 
 public class DropwizardWebServiceApplication extends Application<DropwizardWebServiceConfiguration> {
-    private JobRoleService jobRoleService;
+    private final JobRoleService jobRoleService;
+    private final BandService bandService;
 
     public DropwizardWebServiceApplication() {
         DatabaseConnector databaseConnector = new DatabaseConnector();
         jobRoleService = new JobRoleService(databaseConnector, new JobRoleDao());
+        bandService = new BandService(databaseConnector, new BandDao());
     }
 
     public static void main(final String[] args) throws Exception {
@@ -45,6 +49,6 @@ public class DropwizardWebServiceApplication extends Application<DropwizardWebSe
         // TODO: implement application
         environment.jersey().register(new HelloWorldController());
         environment.jersey().register(new JobRoleController(jobRoleService));
-        environment.jersey().register(new BandController());
+        environment.jersey().register(new BandController(bandService));
     }
 }

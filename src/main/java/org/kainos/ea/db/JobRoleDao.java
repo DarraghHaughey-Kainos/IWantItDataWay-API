f90abec5c5dc7216a7005bf8a3fe09934c06585b
Darragh <darragh.haughey@kainos.com>
2023-11-16 11:33:08 +0000

    Changed imports in dao

--- src/main/java/org/kainos/ea/db/JobRoleDao.java
+++ src/main/java/org/kainos/ea/db/JobRoleDao.java
@@ -3,7 +3,10 @@ package org.kainos.ea.db;
 import org.kainos.ea.cli.JobRole;
 import org.kainos.ea.client.ActionFailedException;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
